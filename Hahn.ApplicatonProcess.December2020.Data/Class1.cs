b624e4f68afb6b01103d3755c6b8a0d86fbb49db
John Esan <jcoool40@gmail.com>
2020-12-30 14:01:59 +0100

    Added projects

---
+++ Hahn.ApplicatonProcess.December2020.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Hahn.ApplicatonProcess.December2020.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Hahn.ApplicatonProcess.December2020.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Hahn.ApplicatonProcess.December2020.Domain
+{
+    public class Class1
+    {
+    }
+}
